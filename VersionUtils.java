1ed2be33f9136f9a173c3a8a67c95dd3d5e0d466
JacksonLi-LiBin <JacksonLi-LiBin@users.noreply.github.com>
2016-06-16 09:57:59 +0800

    Add files via upload

---
+++ VersionUtils.java
@@ -0,0 +1,43 @@
+package com.wuxiaolong.androidutils.library;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class VersionUtils {
+    /**
+     * 获取版本号
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        String packageName = context.getPackageName();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "1.0.0";
+    }
+
+    /**
+     * 获取版本code
+     *
+     * @param context
+     * @return
+     */
+    public static int getVersionCode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        String packageName = context.getPackageName();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+}
