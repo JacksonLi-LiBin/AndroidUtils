3589cb1d17a4d43dc2758b4451ba23fab9d9bd47
JacksonLi-LiBin <JacksonLi-LiBin@users.noreply.github.com>
2016-09-03 16:40:37 +0800

    Update NetConnectUtils.java

--- NetConnectUtils.java
+++ NetConnectUtils.java
@@ -13,14 +13,21 @@ public class NetConnectUtils {
      * @return
      */
     public static boolean isNetConnected(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cm != null) {
-            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-            if (networkInfo != null) {
-                return true;
+        //1、获取ConnectivityManager对象
+        // 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            //2、获取NetworkInfo对象
+            NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+            //3、判断当前网络状态是否为连接状态
+            if (info != null && info.isConnected()) {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    // 当前所连接的网络可用
+                    return true;
+                }
             }
         }
-
         return false;
     }
 
